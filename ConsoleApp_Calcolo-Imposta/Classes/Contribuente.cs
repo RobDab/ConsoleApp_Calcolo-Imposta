1dda76d37cfe2284810d83a37d9d15ef86c06356
RobDab <roberto.dabb@gmail.com>
2022-11-25 17:54:12 +0100

    Aggiungere i file di progetto.

---
+++ ConsoleApp_Calcolo-Imposta/Classes/Contribuente.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Calcolo_Imposta.Classes
+{
+    internal class Contribuente
+    {
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public string FiscalCode { get; set; }
+
+        public string Gender { get; set; }
+        public string City { get; set; }
+
+        public double GrossSalary { get; set; }
+
+        public Contribuente(string CF)
+        {
+            //la string MonthCode permette di confrontare il carattere relativo al mese
+            //del codice fiscale e identificarlo grazie al suo indice in MonthCode > vedi riga 44
+            String MonthCode = "ABCDEHLMPRST";
+            
+
+            // 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
+            // D B B R R T 9 6 M 1 9  A  6  6  2  P
+            
+            string year = $"{CF[6]}{CF[7]}";
+            int Year = int.Parse(year);
+            if (Year > 20)
+            {
+                Year += 1900;
+            }
+            else
+            {
+                Year += 2000;
+            }
+
+
+
+            char month = CF[8];
+            int Month = MonthCode.IndexOf(month) +1;
+
+            string day = $"{CF[9]}{CF[10]}";
+            int dayNum = int.Parse(day);
+            int Day;
+
+            if (dayNum<31) {
+                Day = dayNum;
+                Gender = "male";
+            }
+            else
+            {
+                Day = dayNum - 40;
+                Gender = "female";
+            }
+
+            
+            BirthDate = new DateTime(Year, Month, Day);
+        }
+
+        public double GetImposta()
+        {
+            double Imposta;
+            if(GrossSalary <= 15000)
+            {
+                Imposta = GrossSalary * 23 / 100;
+            }else if(GrossSalary <= 28000)
+            {
+                Imposta = 3450 + ((GrossSalary - 15000) * 27 / 100);
+            }else if(GrossSalary <= 55000)
+            {
+                Imposta = 6960 + ((GrossSalary - 28000) * 38 / 100);
+            }else if (GrossSalary <= 75000)
+            {
+                Imposta = 17220 + ((GrossSalary - 28000) * 41 / 100);
+            }else
+            {
+                Imposta = 25420 + ((GrossSalary - 28000) * 43 / 100);
+            }
+
+            return Imposta;
+        }
+    }
+}
---
+++ ConsoleApp_Calcolo-Imposta/Program.cs
@@ -0,0 +1,116 @@
+﻿using ConsoleApp_Calcolo_Imposta.Classes;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Calcolo_Imposta
+{
+    
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+
+
+            Console.WriteLine("         |       *** Benventuro in IncCool8 ***     |");
+            Console.WriteLine("         |--   Calcolatore d'imposta sul reddito  --|\n\n");
+            Console.WriteLine("_click -ENTER- to start.");
+            Console.ReadLine();
+
+            string answerNeg = "n";
+            string answer = "y";
+            do
+            {
+                try
+                {
+                    Contribuente CurrentContribuente = GetInfo();
+                    Console.WriteLine($"|    Contribuente: {CurrentContribuente.Name} {CurrentContribuente.Lastname} / ({CurrentContribuente.Gender})");
+                    Console.WriteLine($"|    Nato il: {CurrentContribuente.BirthDate.ToShortDateString()}");
+                    Console.WriteLine($"|    Residente a: {CurrentContribuente.City}");
+                    Console.WriteLine($"|    Cod Fisc: {CurrentContribuente.FiscalCode}\n");
+                    Console.WriteLine($">    Reddito dichiarato :  €{CurrentContribuente.GrossSalary}");
+                    Console.WriteLine($">    Imposta sul reddito:  €{CurrentContribuente.GetImposta()}\n");
+
+                    double NetSalary = GetNetSalary(CurrentContribuente.GrossSalary, CurrentContribuente.GetImposta());
+
+                    Console.WriteLine($"*** Reddito netto previsto: €{NetSalary}\n");
+
+                    Console.WriteLine("-* Vuoi effettuare un nuoco calcolo? *-\n qualsiasi tasto per proseguire_\n'n' per terminare_");
+                    answer = Console.ReadLine();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.ReadLine();
+                }
+
+
+                if(answer == answerNeg)
+                {
+                    Console.WriteLine("---------------------\n");
+                    Console.WriteLine(" THANKS FOR USING THIS APPLICATION! rate me on the appstore\n\n ___ Ending in 3s...\n");
+                    Console.WriteLine("---------------------\n");
+                    Thread.Sleep(3500);
+                    Environment.Exit(0);
+                }
+            } while (answer != answerNeg);
+        }
+
+        public static Contribuente GetInfo()
+        {
+            string cf;
+
+            Console.WriteLine("- Insert Name: _");
+            string Name = Console.ReadLine();
+
+            Console.WriteLine("- Insert Lastname: _");
+            string Lastname = Console.ReadLine();
+
+            do
+            {
+                Console.WriteLine("-Insert Fiscal Code: _");
+                cf = Console.ReadLine();
+                if(cf.Length != 16)
+                {
+                    Console.WriteLine("- hai inserito un CF della lunghezza sbagliata. Riprova_\n");
+                }
+            } while (cf.Length != 16);
+
+            Console.WriteLine("-Insert City: _");
+            string City = Console.ReadLine();
+            
+
+            Console.WriteLine("-Insert salary: € _");
+            double salary = double.Parse(Console.ReadLine());
+            Console.WriteLine("... solo?");
+            Console.ReadLine();
+
+
+
+            Contribuente currentContribuente = new Contribuente(cf.ToUpper())
+            {
+                Name = Name.ToUpper(),
+                Lastname = Lastname.ToUpper(),
+                FiscalCode = cf.ToUpper(),
+                City = City,
+                GrossSalary = salary
+            };
+
+            return currentContribuente;
+        }
+
+        public static double GetNetSalary(double gross, double imposta)
+        {
+            double netSalary = gross - imposta;
+
+            return netSalary;
+        }
+    }
+}
